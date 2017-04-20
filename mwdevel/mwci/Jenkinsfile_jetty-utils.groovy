8b240eb396af530451ef4ad3d78c5ee1dbda66fc
Marco Caberletti <marco.caberletti@gmail.com>
2017-04-20 09:21:00 +0200

    Add sonar analysis

--- mwdevel/mwci/Jenkinsfile_jetty-utils.groovy
+++ mwdevel/mwci/Jenkinsfile_jetty-utils.groovy
@@ -14,5 +14,12 @@ node('maven'){
     sh 'sed -i \'s#http:\\/\\/radiohead\\.cnaf\\.infn\\.it:8081\\/nexus\\/content\\/repositories#https:\\/\\/repo\\.cloud\\.cnaf\\.infn\\.it\\/repository#g\' pom.xml'
   }
 
+  stage('analysis'){
+    def cobertura_opts = 'cobertura:cobertura -Dmaven.test.failure.ignore -DfailIfNoTests=false -Dcobertura.report.format=xml'
+    def checkstyle_opts = 'checkstyle:check -Dcheckstyle.config.location=google_checks.xml'
+
+    withSonarQubeEnv{ sh "mvn clean -U ${cobertura_opts} ${checkstyle_opts} ${SONAR_MAVEN_GOAL} -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}" }
+  }
+
   stage('deploy'){ sh "mvn clean -U -B deploy" }
 }
