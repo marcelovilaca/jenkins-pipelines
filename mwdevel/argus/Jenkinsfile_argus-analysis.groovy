452b51327b3a8ed178d1c1ab13bf8cf46aca65e9
Marco Caberletti <marco.caberletti@gmail.com>
2017-04-21 10:27:56 +0200

    Aggregate job results

--- mwdevel/argus/Jenkinsfile_argus-analysis.groovy
+++ mwdevel/argus/Jenkinsfile_argus-analysis.groovy
@@ -7,46 +7,47 @@ properties([
 ])
 
 def argus_repo = 'https://github.com/argus-authz'
+def pap, pdp, pep_server, pdp_pep_common, pep_common, pep_api_java
 
 stage('analyze'){
   parallel(
       "pap" : {
-        build job: 'sonar-maven-analysis',
+        pap = build job: 'sonar-maven-analysis', propagate: false,
         parameters: [
           string(name: 'REPO',           value: "${argus_repo}/argus-pap"),
           string(name: 'BRANCH',         value: '1_7'),
         ]
       },
       "pdp" : {
-        build job: 'sonar-maven-analysis',
+        pdp = build job: 'sonar-maven-analysis', propagate: false,
         parameters: [
           string(name: 'REPO',           value: "${argus_repo}/argus-pdp"),
           string(name: 'BRANCH',         value: '1_7'),
         ]
       },
       "pep-server" : {
-        build job: 'sonar-maven-analysis',
+        pep_server = build job: 'sonar-maven-analysis', propagate: false,
         parameters: [
           string(name: 'REPO',           value: "${argus_repo}/argus-pep-server"),
           string(name: 'BRANCH',         value: 'iota-ca-support'),
         ]
       },
       "pdp-pep-common" : {
-        build job: 'sonar-maven-analysis',
+        pdp_pep_common = build job: 'sonar-maven-analysis', propagate: false,
         parameters: [
           string(name: 'REPO',           value: "${argus_repo}/argus-pdp-pep-common"),
           string(name: 'BRANCH',         value: '1_5'),
         ]
       },
       "pep-common" : {
-        build job: 'sonar-maven-analysis',
+        pep_common = build job: 'sonar-maven-analysis', propagate: false,
         parameters: [
           string(name: 'REPO',           value: "${argus_repo}/argus-pep-common"),
           string(name: 'BRANCH',         value: '2_3'),
         ]
       },
       "pep-api-java" : {
-        build job: 'sonar-maven-analysis',
+        pep_api_java = build job: 'sonar-maven-analysis', propagate: false,
         parameters: [
           string(name: 'REPO',           value: "${argus_repo}/argus-pep-api-java"),
           string(name: 'BRANCH',         value: '2_3'),
@@ -54,5 +55,23 @@ stage('analyze'){
       },
       failFast: false
       )
-  currentBuild.result = 'SUCCESS'
+
+  def jobs = [
+    pap,
+    pdp,
+    pep_server,
+    pdp_pep_common,
+    pep_common,
+    pep_api_java
+  ]
+  def result = 'SUCCESS'
+
+  for(int i=0; i<jobs.size(); i++) {
+    def job_res = jobs.get(i).result
+    if(!'SUCCESS'.equals(job_res)) {
+      result = job_res
+    }
+  }
+
+  currentBuild.result = result
 }
