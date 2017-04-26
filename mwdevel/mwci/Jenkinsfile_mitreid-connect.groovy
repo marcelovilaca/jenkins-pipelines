20bc0639eab4cb8283d3890a7f6a7c39bd89e3df
Marco Caberletti <marco.caberletti@gmail.com>
2017-04-26 10:28:45 +0200

    Add MitreID connect Jenkinsfile

---
+++ mwdevel/mwci/Jenkinsfile_mitreid-connect.groovy
@@ -0,0 +1,25 @@
+#!groovy
+
+properties([
+  buildDiscarder(logRotator(numToKeepStr: '5')),
+  pipelineTriggers([cron('@daily')]),
+  parameters([
+    string(name: 'BRANCH', defaultValue: 'devel', description: '' )
+  ]),
+])
+
+node('maven'){
+  stage('prepare'){
+    git branch: "${params.BRANCH}", url: 'https://github.com/indigo-iam/OpenID-Connect-Java-Spring-Server.git'
+    sh 'sed -i \'s#http:\\/\\/radiohead\\.cnaf\\.infn\\.it:8081\\/nexus\\/content\\/repositories#https:\\/\\/repo\\.cloud\\.cnaf\\.infn\\.it\\/repository#g\' pom.xml'
+  }
+
+  stage('analysis'){
+    def cobertura_opts = 'cobertura:cobertura -Dmaven.test.failure.ignore -DfailIfNoTests=false -Dcobertura.report.format=xml'
+    def checkstyle_opts = 'checkstyle:check -Dcheckstyle.config.location=google_checks.xml'
+
+    withSonarQubeEnv{ sh "mvn clean -U ${cobertura_opts} ${checkstyle_opts} ${SONAR_MAVEN_GOAL} -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}" }
+  }
+
+  stage('deploy'){ sh "mvn -U -B deploy -P cnaf-snapshots" }
+}
