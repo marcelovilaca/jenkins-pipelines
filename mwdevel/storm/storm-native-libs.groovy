04d7b8eb98c3fd0af3c41f6cfdec2fb30014f9f8
Marco Caberletti <marco.caberletti@gmail.com>
2017-05-05 11:20:02 +0200

    Add Slack notifications

--- mwdevel/storm/storm-native-libs.groovy
+++ mwdevel/storm/storm-native-libs.groovy
@@ -1,22 +1,42 @@
-#!groovy
+pipeline {
+  agent { label 'maven' }
 
-properties([
-  buildDiscarder(logRotator(numToKeepStr: '5')),
-  pipelineTriggers([cron('@daily')]),
-])
-
-node('maven'){
-  stage('prepare'){
-    git branch: 'master', url: 'https://github.com/italiangrid/storm-native-libs.git'
-    sh 'sed -i \'s#http:\\/\\/radiohead\\.cnaf\\.infn\\.it:8081\\/nexus\\/content\\/repositories#https:\\/\\/repo\\.cloud\\.cnaf\\.infn\\.it\\/repository#g\' pom.xml'
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+    timeout(time: 1, unit: 'HOURS')
   }
 
-  stage('analysis'){
-    def cobertura_opts = 'cobertura:cobertura -Dmaven.test.failure.ignore -DfailIfNoTests=false -Dcobertura.report.format=xml'
-    def checkstyle_opts = 'checkstyle:check -Dcheckstyle.config.location=google_checks.xml'
+  triggers { cron('@daily') }
 
-    withSonarQubeEnv{ sh "mvn clean -U ${cobertura_opts} ${checkstyle_opts} ${SONAR_MAVEN_GOAL} -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}" }
+  stages {
+    stage('prepare'){
+      steps {
+        git branch: 'master', url: 'https://github.com/italiangrid/storm-native-libs.git'
+        sh 'sed -i \'s#http:\\/\\/radiohead\\.cnaf\\.infn\\.it:8081\\/nexus\\/content\\/repositories#https:\\/\\/repo\\.cloud\\.cnaf\\.infn\\.it\\/repository#g\' pom.xml'
+      }
+    }
+
+    stage('analysis'){
+      steps {
+        script {
+          def cobertura_opts = 'cobertura:cobertura -Dmaven.test.failure.ignore -DfailIfNoTests=false -Dcobertura.report.format=xml'
+          def checkstyle_opts = 'checkstyle:check -Dcheckstyle.config.location=google_checks.xml'
+
+          withSonarQubeEnv{ sh "mvn clean -U ${cobertura_opts} ${checkstyle_opts} ${SONAR_MAVEN_GOAL} -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}" }
+        }
+      }
+    }
+
+    stage('deploy'){ steps { sh "mvn clean -U -B deploy"
+      } }
   }
 
-  stage('deploy'){ sh "mvn clean -U -B deploy" }
+  post {
+    failure {
+      slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
+    }
+    unstable {
+      slackSend color: 'warning', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Unstable (<${env.BUILD_URL}|Open>)"
+    }
+  }
 }
