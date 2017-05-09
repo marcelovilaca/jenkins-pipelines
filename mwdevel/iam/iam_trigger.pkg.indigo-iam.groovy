c5216fe3963c856e028175adb6e6f645b9e20ecd
Marco Caberletti <marco.caberletti@gmail.com>
2017-05-09 13:11:13 +0200

    Add ubuntu packging

--- mwdevel/iam/iam_trigger.pkg.indigo-iam.groovy
+++ mwdevel/iam/iam_trigger.pkg.indigo-iam.groovy
@@ -5,35 +5,45 @@ properties([
   pipelineTriggers([cron('@daily')]),
   parameters([
     string(name: 'PKG_TAG', defaultValue: 'master', description: 'The branch of the pkg.argus repo' ),
-    booleanParam(name:'INCLUDE_PKG_BUILD_NUMBER', defaultValue: true, description: 'When true, creates packages which include a build number in their version.')
+    choice(name: 'INCLUDE_BUILD_NUMBER', choices: '1\n0', description: 'Flag to include/exclude build number.')
   ]),
 ])
 
 
-def component_list = 'iam-login-service'
 def build_number = ''
-def pkg_el7
+def pkg_el7, pkg_deb
 
 try {
-  stage('create RPMs'){
+  stage('create packages'){
 
     if("${params.INCLUDE_PKG_BUILD_NUMBER}" == true) {
       build_number = new Date().format("yyyyMMddHHmmss")
     }
 
-    pkg_el7 = build job: 'pkg.indigo-iam/master', parameters: [
-      string(name: 'COMPONENTS', value: "${component_list}"),
-      string(name: 'PLATFORM', value: 'centos7'),
-      string(name: 'PKG_BUILD_NUMBER', value: "${build_number}")
-    ]
+    parallel(
+        "centos7": {
+          pkg_el7 = build job: "pkg.indigo-iam/${params.PKG_TAG}", parameters: [
+            string(name: 'PKG_BUILD_NUMBER', value: "${build_number}"),
+            string(name: 'INCLUDE_BUILD_NUMBER', value: "${params.INCLUDE_BUILD_NUMBER}"),
+            string(name: 'PLATFORM', value: "centos7")
+          ]
+        },
+        "ubuntu1604": {
+          pkg_deb = build job: "pkg.indigo-iam/${params.PKG_TAG}", parameters: [
+            string(name: 'PKG_BUILD_NUMBER', value: "${build_number}"),
+            string(name: 'INCLUDE_BUILD_NUMBER', value: "${params.INCLUDE_BUILD_NUMBER}"),
+            string(name: 'PLATFORM', value: "ubuntu1604")
+          ]
+        }
+        )
   }
 
-  node('generic'){
-    stage('archive'){
-      def iam_root = "/mnt/packages/repo/indigo-iam"
+  def iam_root = "/mnt/packages/repo/indigo-iam"
 
+  stage('archive RPMs'){
+    node('generic'){
       step ([$class: 'CopyArtifact',
-        projectName: 'pkg.indigo-iam/master',
+        projectName: "pkg.indigo-iam/${params.PKG_TAG}",
         filter: 'repo/centos7/**',
         selector: [$class: 'SpecificBuildSelector', buildNumber: "${pkg_el7.number}"]
       ])
@@ -48,12 +58,36 @@ try {
         sh "mkdir -p ${iam_root}/builds/build_${BUILD_NUMBER}"
         sh "cp -r el7/ ${iam_root}/builds/build_${BUILD_NUMBER}/"
       }
+    }
+  }
 
+  stage('archive DEBs'){
+    node('generic-ubuntu'){
+      step ([$class: 'CopyArtifact',
+        projectName: "pkg.indigo-iam/${params.PKG_TAG}",
+        filter: 'repo/**',
+        selector: [$class: 'SpecificBuildSelector', buildNumber: "${pkg_deb.number}"]
+      ])
+
+      dir('repo') {
+        def debdir = "xenial/amd64"
+        sh "mkdir -p ${debdir}"
+        sh "mv ubuntu1604/*.deb ${debdir}"
+
+        dir('xenial') { sh "dpkg-scanpackages -m amd64 | gzip > amd64/Packages.gz" }
+
+        sh "mkdir -p ${iam_root}/builds/build_${BUILD_NUMBER}"
+        sh "cp -r xenial/ ${iam_root}/builds/build_${BUILD_NUMBER}/"
+      }
+    }
+  }
+
+  stage('update symlink'){
+    node('generic'){
       dir("${iam_root}"){
         sh "rm -vf ${iam_root}/nightly"
         sh "ln -vs ./builds/build_${BUILD_NUMBER}/ nightly"
       }
-
       sh "find ${iam_root}/builds/ -maxdepth 1 -type d -ctime +10 -print -exec rm -rf {} \\;"
     }
   }
