e59c4f28d37517935b45154065dc0246c08d416e
Marco Caberletti <marco.caberletti@gmail.com>
2017-05-09 15:14:15 +0200

    Fix conditional test

--- mwdevel/iam/iam_trigger.pkg.indigo-iam.groovy
+++ mwdevel/iam/iam_trigger.pkg.indigo-iam.groovy
@@ -16,7 +16,7 @@ def pkg_el7, pkg_deb
 try {
   stage('create packages'){
 
-    if("${params.INCLUDE_PKG_BUILD_NUMBER}" == true) {
+    if("${params.INCLUDE_PKG_BUILD_NUMBER}" == "1") {
       build_number = new Date().format("yyyyMMddHHmmss")
     }
 
