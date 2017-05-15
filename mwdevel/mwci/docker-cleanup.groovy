128d70379b1ed539332f52443322902c3ba5cf30
Marco Caberletti <marco.caberletti@gmail.com>
2017-05-15 13:01:37 +0200

    Add cleanup jobs

---
+++ mwdevel/mwci/docker-cleanup.groovy
@@ -0,0 +1,36 @@
+pipeline {
+  agent { label 'docker' }
+
+  options {
+    timeout(time: 1, unit: 'HOURS')
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+
+  triggers { cron('H H/6 * * *') }
+
+  stages {
+    stage('clean'){
+      steps {
+        git 'https://github.com/marcocaberletti/scripts.git'
+
+        echo('Running on:')
+        sh "docker info | grep Name: | tail -n1"
+        sh 'bin/docker-cleanup'
+      }
+    }
+  }
+
+  post {
+    failure {
+      slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
+    }
+
+    changed {
+      script{
+        if('SUCCESS'.equals(currentBuild.result)) {
+          slackSend color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal (<${env.BUILD_URL}|Open>)"
+        }
+      }
+    }
+  }
+}
---
+++ mwdevel/mwci/purge-scratch-area.groovy
@@ -0,0 +1,32 @@
+pipeline {
+  agent { label 'kubectl' }
+
+  options {
+    timeout(time: 1, unit: 'HOURS')
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+
+  triggers { cron('@daily') }
+
+  parameters {
+    string(name: 'DAYS', defaultValue: '30', description: '' )
+  }
+
+  stages {
+    stage('clean'){ steps { sh "find /srv/scratch/ -maxdepth 1 -type d -ctime +${params.DAYS} -print -exec rm -rf {} \\;" } }
+  }
+
+  post {
+    failure {
+      slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
+    }
+
+    changed {
+      script{
+        if('SUCCESS'.equals(currentBuild.result)) {
+          slackSend color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal (<${env.BUILD_URL}|Open>)"
+        }
+      }
+    }
+  }
+}
