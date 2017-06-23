d23c9fb74d321087881b967fa5da93f865381d63
Marco Caberletti <marco.caberletti@gmail.com>
2017-06-23 14:15:26 +0200

    Remove code analysis

--- mwdevel/iam/iam-build.groovy
+++ mwdevel/iam/iam-build.groovy
@@ -5,21 +5,14 @@ properties([
   pipelineTriggers([cron('@daily')]),
   parameters([
     string(name: 'REPO',   defaultValue: 'https://github.com/marcocaberletti/iam.git', description: '' ),
-    string(name: 'BRANCH', defaultValue: 'develop', description: '' )
+    string(name: 'BRANCH', defaultValue: 'mail-tests', description: '' )
   ]),
 ])
 
 try {
-  stage("prepare"){
-    node('generic') {
-      git branch: "${params.BRANCH}", url: "${params.REPO}"
-      stash name: 'code', useDefaultExcludes: false
-    }
-  }
-
   stage('build'){
     node('maven') {
-      unstash 'code'
+      git url: "${params.REPO}", branch: "${params.BRANCH}"
       sh "echo v`sh utils/print-pom-version.sh` > version.txt"
       sh "git rev-parse --short HEAD > version-commit.txt"
       sh "mvn clean package -U -Dmaven.test.failure.ignore"
@@ -31,48 +24,6 @@ try {
       archive 'version.txt'
       archive 'version-commit.txt'
     }
-  }
-
-  stage('code analysis'){
-    def cobertura_opts = 'cobertura:cobertura -Dmaven.test.failure.ignore'
-    def checkstyle_opts = 'checkstyle:check -Dcheckstyle.config.location=google_checks.xml'
-
-    parallel(
-        'coverage' : {
-          node('maven'){
-            unstash 'code'
-            sh "mvn ${cobertura_opts}"
-
-            publishHTML(target: [
-              reportName           : 'Coverage Report',
-              reportDir            : 'iam-login-service/target/site/cobertura/',
-              reportFiles          : 'index.html',
-              keepAll              : true,
-              alwaysLinkToLastBuild: true,
-              allowMissing         : false
-            ])
-          }
-        },
-        'checkstyle' : {
-          node('maven'){
-            unstash 'code'
-            dir('iam-persistence') { sh "mvn clean install" }
-            sh "mvn ${checkstyle_opts}"
-
-            step([$class: 'hudson.plugins.checkstyle.CheckStylePublisher',
-              pattern: '**/checkstyle-result.xml',
-              healty: '20',
-              unHealty: '100'])
-          }
-        },
-        'static analysis': {
-          build job: 'sonar-maven-analysis', propagate: false,
-          parameters: [
-            string(name: 'REPO',   value: "${params.REPO}"),
-            string(name: 'BRANCH', value: "${params.BRANCH}"),
-          ]
-        },
-        )
   }
 }catch(e) {
   slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
