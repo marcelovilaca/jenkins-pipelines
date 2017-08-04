5fc16bbf4f0e4fb2e90a1332d870939a9d1bafff
Marco Caberletti <marco.caberletti@gmail.com>
2017-08-04 09:19:43 +0200

    Add egi-igtf-cas pipeline

--- mwdevel/mwci/egi-igtf-cas-bundle.groovy
+++ mwdevel/mwci/egi-igtf-cas-bundle.groovy
@@ -6,19 +6,36 @@ pipeline {
     buildDiscarder(logRotator(numToKeepStr: '5'))
   }
 
-  triggers { cron('H/20 * * * *') }
+  parameters {
+    string(name: 'SECRET_NAME', defaultValue: 'egi-igtf-cas-secret', description: 'Kube secret name')
+  }
+
+  triggers { cron('@daily') }
 
   environment {
-    REPORT_DIR = "/srv/scratch/${env.BUILD_TAG}/reports"
-    POD_NAME = "ggus-report"
-    POD_FILE = "ggus-report.pod.yaml"
+    OUTPUT_DIR = "/srv/scratch/${env.BUILD_TAG}/"
+    POD_NAME = "egi-igtf-cas"
+    POD_FILE = "egi-igtf-cas.pod.yaml"
   }
 
   stages {
+    stage('build image'){
+      agent {label 'docker'}
+      steps{
+        deleteDir()
+        git 'https://github.com/marcocaberletti/docker'
+        dir('egi-igtf-cas') {
+          sh "./build-image.sh"
+          sh "./push-image.sh"
+        }
+      }
+    }
+
+
     stage('prepare'){
       steps {
         deleteDir()
-        sh "mkdir -p ${env.REPORT_DIR}"
+        sh "mkdir -p ${env.OUTPUT_DIR}"
 
         script {
           def pod_template = """
@@ -37,14 +54,14 @@ spec:
       server: 10.0.0.30
       path: /srv/kubernetes/volumes/scratch
   containers:
-  - name: ggus-report
-    image: ${env.DOCKER_REGISTRY_HOST}/italiangrid/ggus-mon:latest
+  - name: egi-igtf-cas
+    image: ${env.DOCKER_REGISTRY_HOST}/italiangrid/egi-igtf-cas:latest
     volumeMounts:
     - name: scratch-area
       mountPath: /srv/scratch
     env:
-    - name: REPORT_DIR
-      value: ${REPORT_DIR}
+    - name: BUNDLE_DEST_DIR
+      value: ${OUTPUT_DIR}
     - name: TZ
       value: Europe/Rome
 """
@@ -61,16 +78,20 @@ spec:
         sh "kubectl logs -f ${env.POD_NAME}"
       }
 
-      post {  always { sh "kubectl delete -f ${env.POD_FILE}" }  }
+      post {
+        always {   sh "kubectl delete -f ${env.POD_FILE}" }
+      }
     }
 
-    stage('archive'){
+    stage('update secret'){
       steps {
-        script {
-          dir("${env.REPORT_DIR}"){ archiveArtifacts "**" }
-
-          currentBuild.result = 'SUCCESS'
-        }
+        sh """
+          kubectl create secret generic ${params.SECRET_NAME} \\
+            --namespace=default --from-file=ca.crt=${env.OUTPUT_DIR}/tls-ca-bundle.pem \\
+            --dry-run -o yaml | \\
+          kubectl replace secret ${params.SECRET_NAME} -f -
+          """
+        script { currentBuild.result = 'SUCCESS' }
       }
     }
   }
