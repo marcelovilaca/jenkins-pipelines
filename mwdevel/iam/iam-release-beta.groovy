3510af3c7ec6d3935167cc00e8e9f3938229c2b8
Marco Caberletti <marco.caberletti@gmail.com>
2017-08-28 15:26:46 +0200

    Add push to Github stage

--- mwdevel/iam/iam-release-beta.groovy
+++ mwdevel/iam/iam-release-beta.groovy
@@ -3,7 +3,8 @@
 properties([
   buildDiscarder(logRotator(numToKeepStr: '5')),
   parameters([
-    string(name: 'PKG_TAG', defaultValue: 'master', description: ''),
+    string(name: 'PKG_TAG', defaultValue: 'v1.0.0', description: ''),
+    string(name: 'GITHUB_REPO', defaultValue: 'github.com/marcocaberletti/test-lfs-repo', description: '')
   ]),
 ])
 
@@ -35,6 +36,34 @@ try {
       string(name: 'TARGET', value: "${target}"),
       string(name: 'REPO_TITLE', value: "${title}")
     ]
+  }
+
+  stage("Publish to Github"){
+    slackSend color: 'warning', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Requires approval to the next stage (<${env.BUILD_URL}|Open>)"
+
+    timeout(time: 60, unit: 'MINUTES'){
+      approver = input(message: 'Push packages to GitHub repo?', submitterParameter: 'approver')
+    }
+
+    node('generic'){
+      def github_repo_url = "https://${params.GITHUB_REPO}"
+      def github_repo_branch = "master"
+
+      dir('repo'){
+        sh "git clone -b ${github_repo_branch} ${github_repo_branch} ."
+        sh "rsync -avu --delete /mnt/packages/repo/indigo-iam/${target}/ ${target}/"
+
+        withCredentials([
+          usernamePassword(credentialsId: 'marco-github-credentials', passwordVariable: 'git_password', usernameVariable: 'git_username')
+        ]) {
+          sh "git config --global user.name 'JenkinsCI'"
+          sh "git config --global user.email 'jenkinsci@cloud.cnaf.infn.it'"
+          sh "git add ."
+          sh "git commit -m 'Upload ${target} packages for ${params.PKG_TAG}'"
+          sh "git lfs push https://${git_username}:${git_password}@${params.GITHUB_REPO}"
+        }
+      }
+    }
   }
 }catch(e) {
   slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
