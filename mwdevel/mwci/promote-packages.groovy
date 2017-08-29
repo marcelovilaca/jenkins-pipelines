36920fe5f0d77fd241a5845d334048ff76f139bc
Marco Caberletti <marco.caberletti@gmail.com>
2017-08-29 11:49:07 +0200

    Improve rsync options

--- mwdevel/mwci/promote-packages.groovy
+++ mwdevel/mwci/promote-packages.groovy
@@ -27,7 +27,7 @@ pipeline{
             src_dir = "${pkg_root}/builds/build_${params.BUILD_NUMBER}"
           }
 
-          sh "rsync -avu ${src_dir}/ ${dest_dir}/"
+          sh "rsync -av --delete ${src_dir}/ ${dest_dir}/"
         }
       }
     }
