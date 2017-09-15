bc85d9e64110b8fd7e2fa9b01c14c1d3d1ab9659
Marco Caberletti <marco.caberletti@gmail.com>
2017-09-15 14:16:38 +0200

    Rewrite with declarative syntax

--- mwdevel/mwci/sonar-maven-analysis.groovy
+++ mwdevel/mwci/sonar-maven-analysis.groovy
@@ -1,67 +1,5 @@
 #!/usr/bin/env groovy
 
-properties([
-  buildDiscarder(logRotator(numToKeepStr: '10')),
-  parameters([
-    string(name: 'REPO',   defaultValue: '', description: 'Git code repository'),
-    string(name: 'BRANCH', defaultValue: '', description: 'Repository branch'),
-  ])
-])
-
-try {
-  stage('analysis'){
-    node('maven'){
-      git url: "${params.REPO}", branch: "${params.BRANCH}"
-
-      def cobertura_opts = 'cobertura:cobertura -Dmaven.test.failure.ignore -DfailIfNoTests=false -Dcobertura.report.format=xml'
-      def checkstyle_opts = 'checkstyle:check -Dcheckstyle.config.location=google_checks.xml'
-
-      withSonarQubeEnv{ sh "mvn clean -U ${cobertura_opts} ${checkstyle_opts} ${SONAR_MAVEN_GOAL} -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}" }
-
-      dir('target/sonar') {
-        stash name: 'sonar-report', include: 'report-task.txt'
-      }
-    }
-  }
-
-  stage('quality gate'){
-    node('generic'){
-      unstash 'sonar-report'
-
-      def sonarServerUrl = readProperty('report-task.txt', 'serverUrl')
-      def ceTaskUrl = readProperty('report-task.txt', 'ceTaskUrl')
-      def sonarBasicAuth
-
-      withSonarQubeEnv{ sonarBasicAuth  = "${SONAR_AUTH_TOKEN}:" }
-
-      timeout(time: 3, unit: 'MINUTES') {
-        waitUntil {
-          def result = jsonParse(ceTaskUrl, sonarBasicAuth, '.task.status')
-          echo "Current CeTask status: ${result}"
-          return "SUCCESS" == "${result}"
-        }
-      }
-
-      def analysisId = jsonParse(ceTaskUrl, sonarBasicAuth, '.task.analysisId')
-      echo "Analysis ID: ${analysisId}"
-
-      def url = "${sonarServerUrl}/api/qualitygates/project_status?analysisId=${analysisId}"
-      def qualityGate =  jsonParse(url, sonarBasicAuth, '')
-      echo "${qualityGate}"
-
-      def status =  jsonParse(url, sonarBasicAuth, '.projectStatus.status')
-
-      if ("ERROR" == "${status}") {
-        currentBuild.result = 'UNSTABLE'
-        slackSend color: 'warning', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Unstable (<${env.BUILD_URL}|Open>)"
-      }
-    }
-  }
-}catch(e) {
-  slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
-  throw(e)
-}
-
 def readProperty(filename, prop) {
   def value = sh script: "cat ${filename} | grep ${prop} | cut -d'=' -f2-", returnStdout: true
   return value.trim()
@@ -70,4 +8,88 @@ def readProperty(filename, prop) {
 def jsonParse(url, basicAuth, field) {
   def value =  sh script: "curl -s -u '${basicAuth}' '${url}' | jq -r '${field}'", returnStdout: true
   return value.trim()
+}
+
+pipeline {
+  agent none
+
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '10'))
+  }
+
+  parameters {
+    string(name: 'REPO',   defaultValue: '', description: 'Git code repository')
+    string(name: 'BRANCH', defaultValue: '', description: 'Repository branch')
+  }
+
+  stages {
+    stage('analysis') {
+      agent { label 'maven' }
+      steps {
+        git url: "${params.REPO}", branch: "${params.BRANCH}"
+
+        script {
+          def cobertura_opts = 'cobertura:cobertura -Dmaven.test.failure.ignore -DfailIfNoTests=false -Dcobertura.report.format=xml'
+          def checkstyle_opts = 'checkstyle:check -Dcheckstyle.config.location=google_checks.xml'
+
+          withSonarQubeEnv{ sh "mvn clean -U ${cobertura_opts} ${checkstyle_opts} ${SONAR_MAVEN_GOAL} -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}" }
+        }
+        dir('target/sonar') {
+          stash name: 'sonar-report', include: 'report-task.txt'
+        }
+      }
+    }
+
+    stage('quality gate'){
+      agent { label 'generic' }
+      steps {
+        script {
+          unstash 'sonar-report'
+
+          def sonarServerUrl = readProperty('report-task.txt', 'serverUrl')
+          def ceTaskUrl = readProperty('report-task.txt', 'ceTaskUrl')
+          def sonarBasicAuth
+
+          withSonarQubeEnv{ sonarBasicAuth  = "${SONAR_AUTH_TOKEN}:" }
+
+          timeout(time: 3, unit: 'MINUTES') {
+            waitUntil {
+              def result = jsonParse(ceTaskUrl, sonarBasicAuth, '.task.status')
+              echo "Current CeTask status: ${result}"
+              return "SUCCESS" == "${result}"
+            }
+          }
+
+          def analysisId = jsonParse(ceTaskUrl, sonarBasicAuth, '.task.analysisId')
+          echo "Analysis ID: ${analysisId}"
+
+          def url = "${sonarServerUrl}/api/qualitygates/project_status?analysisId=${analysisId}"
+          def qualityGate =  jsonParse(url, sonarBasicAuth, '')
+          echo "${qualityGate}"
+
+          def status =  jsonParse(url, sonarBasicAuth, '.projectStatus.status')
+
+          if ("ERROR" == "${status}") {
+            currentBuild.result = 'UNSTABLE'
+          }
+        }
+      }
+    }
+  }
+
+  post {
+    failure {
+      slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
+    }
+    unstable {
+      slackSend color: 'warning', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Unstable (<${env.BUILD_URL}|Open>)"
+    }
+    changed {
+      script{
+        if('SUCCESS'.equals(currentBuild.result)) {
+          slackSend color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal (<${env.BUILD_URL}|Open>)"
+        }
+      }
+    }
+  }
 }
--- mwdevel/mwci/sonar-scanner-analysis.groovy
+++ mwdevel/mwci/sonar-scanner-analysis.groovy
@@ -1,73 +1,5 @@
 #!/usr/bin/env groovy
 
-properties([
-  buildDiscarder(logRotator(numToKeepStr: '10')),
-  parameters([
-    string(name: 'REPO',           defaultValue: 'https://github.com/marcocaberletti/puppet'),
-    string(name: 'BRANCH',         defaultValue: 'master'),
-    string(name: 'PROJECTKEY',     defaultValue: 'puppet'),
-    string(name: 'PROJECTNAME',    defaultValue: 'Puppet Modules'),
-    string(name: 'PROJECTVERSION', defaultValue: '1.0'),
-    string(name: 'SOURCES',        defaultValue: 'modules')
-  ])
-])
-
-
-node('generic'){
-  try {
-    stage('analyze'){
-      git url: "${params.REPO}", branch: "${params.BRANCH}"
-
-      withSonarQubeEnv{
-        def sonar_opts="-Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}"
-        def project_opts="-Dsonar.projectKey=${params.PROJECTKEY} -Dsonar.projectName='${params.PROJECTNAME}' -Dsonar.projectVersion=${params.PROJECTVERSION} -Dsonar.sources=${params.SOURCES}"
-
-        sh "/opt/sonar-scanner/bin/sonar-scanner ${sonar_opts} ${project_opts}"
-
-        dir('.scannerwork') {
-          stash name: 'sonar-report', include: 'report-task.txt'
-        }
-      }
-    }
-
-    stage('quality gate'){
-      unstash 'sonar-report'
-
-      def sonarServerUrl = readProperty('report-task.txt', 'serverUrl')
-      def ceTaskUrl = readProperty('report-task.txt', 'ceTaskUrl')
-      def sonarBasicAuth
-
-      withSonarQubeEnv{ sonarBasicAuth  = "${SONAR_AUTH_TOKEN}:" }
-
-      timeout(time: 3, unit: 'MINUTES') {
-        waitUntil {
-          def result = jsonParse(ceTaskUrl, sonarBasicAuth, '.task.status')
-          echo "Current CeTask status: ${result}"
-          return "SUCCESS" == "${result}"
-        }
-      }
-
-      def analysisId = jsonParse(ceTaskUrl, sonarBasicAuth, '.task.analysisId')
-      echo "Analysis ID: ${analysisId}"
-
-      def url = "${sonarServerUrl}/api/qualitygates/project_status?analysisId=${analysisId}"
-      def qualityGate =  jsonParse(url, sonarBasicAuth, '')
-      echo "${qualityGate}"
-
-      def status =  jsonParse(url, sonarBasicAuth, '.projectStatus.status')
-
-      if ("ERROR" == "${status}") {
-        currentBuild.result = 'UNSTABLE'
-        slackSend color: 'warning', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Unstable (<${env.BUILD_URL}|Open>)"
-      }
-    }
-  }catch(e) {
-    slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
-    throw(e)
-  }
-}
-
-
 def readProperty(filename, prop) {
   def value = sh script: "cat ${filename} | grep ${prop} | cut -d'=' -f2-", returnStdout: true
   return value.trim()
@@ -76,4 +8,94 @@ def readProperty(filename, prop) {
 def jsonParse(url, basicAuth, field) {
   def value =  sh script: "curl -s -u '${basicAuth}' '${url}' | jq -r '${field}'", returnStdout: true
   return value.trim()
+}
+
+
+pipeline {
+  agent { label 'generic' }
+
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '10'))
+  }
+
+  parameters {
+    string(name: 'REPO',           defaultValue: 'https://github.com/marcocaberletti/puppet')
+    string(name: 'BRANCH',         defaultValue: 'master')
+    string(name: 'PROJECTKEY',     defaultValue: 'puppet')
+    string(name: 'PROJECTNAME',    defaultValue: 'Puppet Modules')
+    string(name: 'PROJECTVERSION', defaultValue: '1.0')
+    string(name: 'SOURCES',        defaultValue: 'modules')
+  }
+
+  stages {
+    stage('analysis'){
+      steps {
+        git url: "${params.REPO}", branch: "${params.BRANCH}"
+
+        script {
+          withSonarQubeEnv{
+            def sonar_opts="-Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}"
+            def project_opts="-Dsonar.projectKey=${params.PROJECTKEY} -Dsonar.projectName='${params.PROJECTNAME}' -Dsonar.projectVersion=${params.PROJECTVERSION} -Dsonar.sources=${params.SOURCES}"
+
+            sh "/opt/sonar-scanner/bin/sonar-scanner ${sonar_opts} ${project_opts}"
+          }
+        }
+
+        dir('.scannerwork') {
+          stash name: 'sonar-report', include: 'report-task.txt'
+        }
+      }
+    }
+
+    stage('quality gate'){
+      steps {
+        script {
+          unstash 'sonar-report'
+
+          def sonarServerUrl = readProperty('report-task.txt', 'serverUrl')
+          def ceTaskUrl = readProperty('report-task.txt', 'ceTaskUrl')
+          def sonarBasicAuth
+
+          withSonarQubeEnv{ sonarBasicAuth  = "${SONAR_AUTH_TOKEN}:" }
+
+          timeout(time: 3, unit: 'MINUTES') {
+            waitUntil {
+              def result = jsonParse(ceTaskUrl, sonarBasicAuth, '.task.status')
+              echo "Current CeTask status: ${result}"
+              return "SUCCESS" == "${result}"
+            }
+          }
+
+          def analysisId = jsonParse(ceTaskUrl, sonarBasicAuth, '.task.analysisId')
+          echo "Analysis ID: ${analysisId}"
+
+          def url = "${sonarServerUrl}/api/qualitygates/project_status?analysisId=${analysisId}"
+          def qualityGate =  jsonParse(url, sonarBasicAuth, '')
+          echo "${qualityGate}"
+
+          def status =  jsonParse(url, sonarBasicAuth, '.projectStatus.status')
+
+          if ("ERROR" == "${status}") {
+            currentBuild.result = 'UNSTABLE'
+          }
+        }
+      }
+    }
+  }
+
+  post {
+    failure {
+      slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
+    }
+    unstable {
+      slackSend color: 'warning', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Unstable (<${env.BUILD_URL}|Open>)"
+    }
+    changed {
+      script{
+        if('SUCCESS'.equals(currentBuild.result)) {
+          slackSend color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal (<${env.BUILD_URL}|Open>)"
+        }
+      }
+    }
+  }
 }
