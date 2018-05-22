1f9fd45ec2317d6117e733d3916cde8a7f6e6bb2
enricovianello <maverick17586@gmail.com>
2018-05-22 09:58:50 +0200

    Added StoRM nightly groovy script

---
+++ mwdevel/storm/storm-nightly.groovy
@@ -0,0 +1,96 @@
+#!/usr/bin/env groovy
+
+def pkg_build_number = 'nightly'
+def pkg_el6
+
+pipeline {
+  agent none
+
+  options {
+    timeout(time: 3, unit: 'HOURS')
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+
+  triggers {
+    cron('@midnight')
+  }
+
+  parameters {
+    string(name: 'PKG_TAG', defaultValue: 'develop', description: 'The branch of the pkg.storm repo' )
+  }
+
+  environment {
+    JOB_NAME = 'pkg.storm'
+    INCLUDE_BUILD_NUMBER='1'
+    NEXUS_URL="http://nexus.default.svc.cluster.local" 
+  }
+
+  stages {
+    stage('create RPM'){
+      steps{
+        script{
+          pkg_el6 = build job: "${env.JOB_NAME}/${params.PKG_TAG}", parameters: [
+            string(name: 'PKG_BUILD_NUMBER', value: "${pkg_build_number}"),
+            string(name: 'INCLUDE_BUILD_NUMBER', value: "${env.INCLUDE_BUILD_NUMBER}"),
+            string(name: 'PLATFORM', value: "centos6")
+          ]
+        }
+      }
+    }
+
+    stage('prepare RPM repo'){
+      agent { label 'generic' }
+      steps {
+        container('generic-runner'){
+          script {
+            step ([$class: 'CopyArtifact',
+              projectName: "${env.JOB_NAME}/${params.PKG_TAG}",
+              filter: 'rpms/centos6/**',
+              selector: [$class: 'SpecificBuildSelector', buildNumber: "${pkg_el6.number}"]
+            ])
+
+            dir('rpms') {
+              sh "mkdir -p el6/x86_64"
+              sh "mv centos6/*.rpm el6/x86_64/"
+              sh "createrepo el6/x86_64/"
+              sh "repoview el6/x86_64/"
+              sh "rm -rf el6/x86_64/repodata"
+              stash includes: 'el6/', name: 'rpm'
+            }
+          }
+        }
+      }
+    }
+
+    stage('push to Nexus'){
+      agent { label 'generic' }
+      steps {
+      	container('generic-runner'){
+		  deleteDir()
+		  unstash 'rpm'
+
+          withCredentials([
+            usernamePassword(credentialsId: 'jenkins-nexus', passwordVariable: 'password', usernameVariable: 'username')
+          ]) {
+            sh "nexus-assets-remove -u ${username} -p ${password} -H ${env.NEXUS_URL} -r storm -q nightly/"
+            sh "nexus-assets-upload -u ${username} -p ${password} -H ${env.NEXUS_URL} -r storm/nightly -d ."
+          }
+        }
+      }
+    }
+  }
+
+  post {
+    failure {
+      slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
+    }
+
+    changed {
+      script{
+        if('SUCCESS'.equals(currentBuild.currentResult)) {
+          slackSend color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal (<${env.BUILD_URL}|Open>)"
+        }
+      }
+    }
+  }
+}
