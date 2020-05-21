bec85ffdad20f5baff6d3d139fc8cbb4bd659242
enricovianello <maverick17586@gmail.com>
2020-05-21 10:23:05 +0200

    Updated storm release beta script

--- mwdevel/storm/storm-release-beta.groovy
+++ mwdevel/storm/storm-release-beta.groovy
@@ -1,160 +1,43 @@
-#!/usr/bin/env groovy
-
-@Library('sd')_
-def kubeLabel = getKubeLabel()
-
-def pkg_el6
-def pkg_el7
-
 pipeline {
-
   agent {
-    kubernetes {
-      label "${kubeLabel}"
-      cloud 'Kube mwdevel'
-      defaultContainer 'runner'
-      inheritFrom 'ci-template'
-    }
-  }
-
-  options {
-    timeout(time: 3, unit: 'HOURS')
-    buildDiscarder(logRotator(numToKeepStr: '5'))
+    label 'docker'
   }
 
   parameters {
-    string(name: 'PKG_TAG_EL6', defaultValue: 'release-el6-1-11-16', description: 'The branch of the EL6 pkg.storm repo')
-    string(name: 'PKG_TAG_EL7', defaultValue: 'release-el7-1-11-16', description: 'The branch of the EL7 pkg.storm repo')
+    string(name: 'PKG_STORM_BRANCH', defaultValue: 'v1.11.18', description: 'The branch of the pkg.storm repo')
   }
 
-  environment {
-    JOB_NAME = 'pkg.storm'
-    NEXUS_URL="http://nexus.default.svc.cluster.local"
+  options {
+    timestamps()
+    ansiColor('xterm')
+    timeout(time: 1, unit: 'HOURS')
+    buildDiscarder(logRotator(numToKeepStr: '5'))
   }
-
+    
   stages {
-    stage('create EL6 RPMs') {
-      steps {
-        script {
-          pkg_el6 = build job: "${env.JOB_NAME}/${params.PKG_TAG_EL6}", parameters: [
-            string(name: 'INCLUDE_BUILD_NUMBER', value: "")
-          ]
-        }
+      
+    stage('build packages'){
+      steps{
+        build "pkg.storm/${params.PKG_STORM_BRANCH}"
       }
     }
-
-    stage('prepare EL6 RPM repo') {
+      
+    stage('update beta repos'){
       steps {
-        script {
-          step ([$class: 'CopyArtifact',
-            projectName: "${env.JOB_NAME}/${params.PKG_TAG_EL6}",
-            filter: 'rpms/centos6/**',
-            selector: [$class: 'SpecificBuildSelector', buildNumber: "${pkg_el6.number}"]
-          ])
-
-          dir('rpms') {
-            sh "mkdir -p el6/x86_64"
-            sh "mv centos6/*.rpm el6/x86_64/"
-            sh "createrepo el6/x86_64/"
-            sh "repoview el6/x86_64/"
-            stash includes: 'el6/', name: 'rpm6'
-          }
-        }
+        build 'release.storm/beta'
       }
     }
-
-    stage('create EL6 repo file') {
-      steps {
-        script {
-          def repoStr = """[storm-beta-centos6]
-name=storm-beta-centos6
-baseurl=https://repo.cloud.cnaf.infn.it/repository/storm/beta/el6/x86_64/
-protect=1
-enabled=1
-priority=1
-gpgcheck=0
-"""
-          writeFile file: "storm-beta-centos6.repo", text: "${repoStr}"
-        }
-        stash includes: '*.repo', name: 'repo6'
-      }
-    }
-
-    stage('create EL7 RPMs') {
-      steps {
-        script {
-          pkg_el7 = build job: "${env.JOB_NAME}/${params.PKG_TAG_EL7}", parameters: [
-            string(name: 'INCLUDE_BUILD_NUMBER', value: "")
-          ]
-        }
-      }
-    }
-
-    stage('prepare EL7 RPM repo') {
-      steps {
-        script {
-          step ([$class: 'CopyArtifact',
-            projectName: "${env.JOB_NAME}/${params.PKG_TAG_EL7}",
-            filter: 'rpms/centos7/**',
-            selector: [$class: 'SpecificBuildSelector', buildNumber: "${pkg_el7.number}"]
-          ])
-
-          dir('rpms') {
-            sh "mkdir -p el7/x86_64"
-            sh "mv centos7/*.rpm el7/x86_64/"
-            sh "createrepo el7/x86_64/"
-            sh "repoview el7/x86_64/"
-            stash includes: 'el7/', name: 'rpm7'
-          }
-        }
-      }
-    }
-
-    stage('create EL7 repo file') {
-      steps {
-        script {
-          def repoStr = """[storm-beta-centos7]
-name=storm-beta-centos7
-baseurl=https://repo.cloud.cnaf.infn.it/repository/storm/beta/el7/x86_64/
-protect=1
-enabled=1
-priority=1
-gpgcheck=0
-"""
-          writeFile file: "storm-beta-centos7.repo", text: "${repoStr}"
-        }
-        stash includes: '*.repo', name: 'repo7'
-      }
-    }
-
-    stage('push to Nexus') {
-      steps {
-        deleteDir()
-        unstash 'rpm6'
-        unstash 'repo6'
-        unstash 'rpm7'
-        unstash 'repo7'
-
-        withCredentials([
-          usernamePassword(credentialsId: 'jenkins-nexus', passwordVariable: 'password', usernameVariable: 'username')
-        ]) {
-          sh "nexus-assets-remove -u ${username} -p ${password} -H ${env.NEXUS_URL} -r storm -q beta/"
-          sh "nexus-assets-upload -u ${username} -p ${password} -H ${env.NEXUS_URL} -r storm/beta -d ."
-        }
-      }
-    }
-
   }
 
   post {
     failure {
-      slackSend color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
+      slackSend channel: '#storm', color: 'danger', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure (<${env.BUILD_URL}|Open>)"
     }
-
+    
     changed {
-      script {
-        if ('SUCCESS'.equals(currentBuild.currentResult)) {
-          slackSend color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal (<${env.BUILD_URL}|Open>)"
+      script{
+        if('SUCCESS'.equals(currentBuild.currentResult)) {
+          slackSend channel: '#storm', color: 'good', message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal (<${env.BUILD_URL}|Open>)"
         }
       }
     }
