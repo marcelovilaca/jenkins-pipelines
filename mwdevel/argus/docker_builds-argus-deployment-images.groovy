5d104fc458a1d9e0eb8099591feee4d5505620af
enricovianello <maverick17586@gmail.com>
2020-07-30 19:09:28 +0200

    Removed useless env var

--- mwdevel/argus/docker_builds-argus-deployment-images.groovy
+++ mwdevel/argus/docker_builds-argus-deployment-images.groovy
@@ -22,10 +22,6 @@ pipeline {
 
   triggers { cron('@daily') }
   
-  environment {
-    DOCKER_REGISTRY_HOST = "${env.DOCKER_REGISTRY_HOST}"
-  }
-
   stages {
     stage('prepare') {
       steps {
